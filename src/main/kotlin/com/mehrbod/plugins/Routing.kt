9f8f31806aaa311109e9360c537223c79ffcd522
Mehrbod <mehrbod.kh@gmail.com>
2022-11-12 09:46:20 +0100

    ref: remove unused routes

--- src/main/kotlin/com/mehrbod/plugins/Routing.kt
+++ src/main/kotlin/com/mehrbod/plugins/Routing.kt
@@ -1,32 +1,11 @@
 package com.mehrbod.plugins
 
 import com.mehrbod.routing.creationRouting
-import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         creationRouting()
-        testRoute()
-        anotherRoute()
-    }
-}
-
-fun Route.testRoute() {
-    route("/test") {
-        get {
-            call.respondText("Hello World!")
-        }
-    }
-}
-
-fun Route.anotherRoute() {
-    route("/test2") {
-        get {
-            call.respondText("Hello World!")
-        }
     }
 }
