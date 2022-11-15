d2a1940ce07eed3b997a52f684694e05c21514cc
Mehrbod <mehrbod.kh@gmail.com>
2022-11-15 13:28:36 +0100

    ref: remove unused imports

--- src/main/kotlin/com/mehrbod/routing/RedirectRouting.kt
+++ src/main/kotlin/com/mehrbod/routing/RedirectRouting.kt
@@ -3,8 +3,6 @@ package com.mehrbod.routing
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import java.math.BigInteger
-import java.security.MessageDigest
 
 fun Route.redirectRoute() {
     get("/{link}") {
