3d3d32218601333099af805d7c194bfb3d8ff1cb
Mehrbod <mehrbod.kh@gmail.com>
2022-11-15 14:05:29 +0100

    chore: add connection pooling dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val exposed_version: String by project
+val hikaricp_version: String by project
 
 plugins {
     application
@@ -29,6 +30,7 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("com.zaxxer:HikariCP:$hikaricp_version")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
